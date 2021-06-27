4087b7629095ccc9a3fd7f14a276d4ee2cf6a3d5
Janam Zaveri <janamzaveri@Janams-MacBook-Air.local>
2021-06-27 16:26:50 +0100

    formatted code.

--- main.cpp
+++ main.cpp
@@ -3,57 +3,52 @@
 #include <fstream>
 #include <string>
 #include <stdlib.h>
-    using namespace std;
-int main(){
-
-
-int choice;
-person p;
-
-while(1){
-cout<<"Address book"<<endl<<endl;
-cout<<" Press (1) and hit ENTER to add a new contact"<<endl
-    <<"Press (2) and hit ENTER to remove a contact"<<endl
-    <<"Press (3) and hit ENTER to search for contact"<<endl<<endl;
-cout<<"Press (4) and hit ENTER to read "<<endl;
-cout<<"Press (5) and hit ENTER to quit"<<endl;
-cin >>choice;
-if (choice==5){
-    break;
-}
-
-
-switch (choice)
+using namespace std;
+int main()
 {
-    case 1:
-        p.get_full_details();
-        add(p);
-        break;
-    case 2:
-        p.get_details();
-        remove(p);
-        break;
-    case 3:
-        p.get_details();
-        
-        search(p);
-        break;
-    case 4:
-        read();
-        break;
 
- 
-    default:
-        cout<<"Please enter a valid option."<<endl;
-}
+    int choice;
+    person p;
 
-}
+    while (1)
+    {
+        cout << "Address book" << endl
+             << endl;
+        cout << " Press (1) and hit ENTER to add a new contact" << endl
+             << "Press (2) and hit ENTER to remove a contact" << endl
+             << "Press (3) and hit ENTER to search for contact" << endl
+             << endl;
+        cout << "Press (4) and hit ENTER to read " << endl;
+        cout << "Press (5) and hit ENTER to quit" << endl;
+        cin >> choice;
+        if (choice == 5)
+        {
+            break;
+        }
 
+        switch (choice)
+        {
+        case 1:
+            p.get_full_details();
+            add(p);
+            break;
+        case 2:
+            p.get_details();
+            remove(p);
+            break;
+        case 3:
+            p.get_details();
 
+            search(p);
+            break;
+        case 4:
+            read();
+            break;
 
+        default:
+            cout << "Please enter a valid option." << endl;
+        }
+    }
 
-
-
-
-return 0;
+    return 0;
 }
--- methods.cpp
+++ methods.cpp
@@ -1,163 +1,128 @@
 #include "header.h"
 #include <iostream>
-#include<string>
-#include<fstream>
+#include <string>
+#include <fstream>
 using namespace std;
 
+void person::get_details(void)
+{
+    cout << "Enter your first Name:" << endl;
+    cin >> f_name;
+    cout << "Enter your surname:" << endl;
+    cin >> s_name;
+    cout << "Enter your email" << endl;
+    cin >> email;
+    cout << "Enter your Telephone number" << endl;
+    cin >> telno;
+}
+void person::get_full_details(void)
+{
 
-    
-    void person::get_details(void)
-    {
-    cout<< "Enter your first Name:"<<endl;
-    cin>>f_name;
-    cout<< "Enter your surname:"<<endl;
-    cin>>s_name;
-    cout<< "Enter your email"<<endl;
-    cin>>email;
-    cout<< "Enter your Telephone number"<<endl;
-    cin>>telno;
-
-
-
-    }
-    void person::get_full_details(void){
-
-        get_details();    
-    cout<< "Enter street"<<endl;
-    cin>>street;
-    cout<< "Enter town"<<endl;
-    cin>>town;
-    cout<< "Enter country"<<endl;
-    cin>>country;
-
-    }
-    
-
-
+    get_details();
+    cout << "Enter street" << endl;
+    cin >> street;
+    cout << "Enter town" << endl;
+    cin >> town;
+    cout << "Enter country" << endl;
+    cin >> country;
+}
 
 void person::display_details(void)
 {
-    cout<< "first name:"<<f_name<<endl;
-    cout<< "surname:"<<s_name<<endl;
-    cout<< "email:"<<email<<endl;
-    cout<< "telno:"<<telno<<endl;
-    cout<< "street:"<<street<<endl;
-    cout<< "town:"<<town<<endl;
-    cout<< "country:"<<country<<endl;
-    cout<<endl;
-
-
+    cout << "first name:" << f_name << endl;
+    cout << "surname:" << s_name << endl;
+    cout << "email:" << email << endl;
+    cout << "telno:" << telno << endl;
+    cout << "street:" << street << endl;
+    cout << "town:" << town << endl;
+    cout << "country:" << country << endl;
+    cout << endl;
 }
-int compare(person p1,person p2){
+int compare(person p1, person p2)
+{
 
-    return strcmp(p1.f_name,p2.f_name)==0 && strcmp( p1.s_name,p2.s_name)==0 && strcmp(p1.email,p2.email)==0 && strcmp(p1.telno,p2.telno)==0;
-
-
-
+    return strcmp(p1.f_name, p2.f_name) == 0 && strcmp(p1.s_name, p2.s_name) == 0 && strcmp(p1.email, p2.email) == 0 && strcmp(p1.telno, p2.telno) == 0;
 }
 
-void  add(person p){
+void add(person p)
+{
 
-    
-    ofstream fout("book.dat", ios::out | ios::binary|ios::app);
-    
-    fout.write((char *) &p, sizeof(p));
+    ofstream fout("book.dat", ios::out | ios::binary | ios::app);
+
+    fout.write((char *)&p, sizeof(p));
 
     fout.close();
-
-
-
-
-
-
 }
-void read(){
+void read()
+{
 
-person p;
-ifstream fin("book.dat",ios::in | ios::binary);
-while(1){
+    person p;
+    ifstream fin("book.dat", ios::in | ios::binary);
+    while (1)
+    {
 
-    if( !fin.read((char *)&p,sizeof(p))){
-        break;
-    }
+        if (!fin.read((char *)&p, sizeof(p)))
+        {
+            break;
+        }
 
-   
-
-    p.display_details();
-
-
-
-
-}
-
-
-}
-
-void search(person p){
-person p1;
-ifstream fin("book.dat",ios::in | ios::binary);
-
-int flag=0;
-while(fin.read((char *)&p1,sizeof(p1))){
-
-    
-
-    if (compare(p1,p)==1){
-        flag=1;
-
-        p1.display_details();
-        break;
-
-
-
-
-
+        p.display_details();
     }
 }
 
-if (flag==0){
+void search(person p)
+{
+    person p1;
+    ifstream fin("book.dat", ios::in | ios::binary);
 
+    int flag = 0;
+    while (fin.read((char *)&p1, sizeof(p1)))
+    {
 
-    cout<<"Not found"<<endl;
-}
-fin.close();
+        if (compare(p1, p) == 1)
+        {
+            flag = 1;
 
-}
+            p1.display_details();
+            break;
+        }
+    }
 
-void remove(person p){
-  person p1;
-  int flag=0;
-   ofstream fout ("temp.dat",ios::binary | ios::app );
-   ifstream fin ("book.dat",ios::binary);
-   while(fin.read((char*)&p1,sizeof(p1))){
-     if(compare(p,p1)==0){
-        fout.write((char*)&p1,sizeof(p1));
+    if (flag == 0)
+    {
 
-
-     }
-
-     else{
-
-
-         flag=1;
-     }
-
-   }
-
-   if (flag==0){
-
-
-       cout<<"Not found.";
-   }
-   fin.close();
-   fout.close();
-   remove("book.dat");
-   rename("temp.dat","book.dat");
-
-
-
-
-
+        cout << "Not found" << endl;
+    }
+    fin.close();
 }
 
+void remove(person p)
+{
+    person p1;
+    int flag = 0;
+    ofstream fout("temp.dat", ios::binary | ios::app);
+    ifstream fin("book.dat", ios::binary);
+    while (fin.read((char *)&p1, sizeof(p1)))
+    {
+        if (compare(p, p1) == 0)
+        {
+            fout.write((char *)&p1, sizeof(p1));
+        }
 
+        else
+        {
+
+            flag = 1;
+        }
+    }
+
+    if (flag == 0)
+    {
+
+        cout << "Not found.";
+    }
+    fin.close();
+    fout.close();
+    remove("book.dat");
+    rename("temp.dat", "book.dat");
+}
